0b4ea5b6c0cac6b5ad6f90044bcbdb9b62e03842
Joel Niklaus <me@joelniklaus.ch>
2015-02-15 11:05:53 +0100

    Revert "view controllers in xml config file added"
    This reverts commit 76027d8e91e41fb78abe3cfb868aef5a81fecb76.

--- src/main/java/ch/joelniklaus/ecogame/controller/IndexController.java
+++ src/main/java/ch/joelniklaus/ecogame/controller/IndexController.java
@@ -1,18 +1,40 @@
 package ch.joelniklaus.ecogame.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class IndexController extends ParentController {
 
-	@RequestMapping(value = "/security-error", method = RequestMethod.GET)
-	public String securityError(RedirectAttributes redirectAttributes) {
-		redirectAttributes.addFlashAttribute("page_error",
-				"You do have have permission to do that!");
-
-		return "redirect:/";
+	@RequestMapping(value = { "", "/" })
+	public String redirect() {
+		return "index";
 	}
+	
+	@RequestMapping(value = "index")
+	public void index(Model model) {
+	}
+	
+	/**
+	 * Shows a 404 error page.
+	 *
+	 * @return
+	 */
+	@RequestMapping(value = "notFound")
+	public void notFound(Model model) {
+	}
+
+	// /**
+	// *
+	// * @param redirectAttributes
+	// * @return
+	// */
+	// @RequestMapping(value = "/security-error", method = RequestMethod.GET)
+	// public String securityError(RedirectAttributes redirectAttributes) {
+	// redirectAttributes.addFlashAttribute("page_error",
+	// "You do have have permission to do that!");
+	//
+	// return "redirect:/";
+	// }
 }
