eff2017b7a4a2329c75c7328e30245adb89b0bc9
Joel Niklaus <me@joelniklaus.ch>
2015-05-03 15:11:22 +0200

    test correction

--- src/test/java/ch/joelniklaus/ecogame/service/LoginServiceTest.java
+++ src/test/java/ch/joelniklaus/ecogame/service/LoginServiceTest.java
@@ -10,26 +10,26 @@ import ch.joelniklaus.ecogame.controller.service.AuthenticationService;
 import ch.joelniklaus.ecogame.controller.service.AuthenticationServiceImpl;
 
 public class LoginServiceTest {
-	
+
 	private AuthenticationService authService;
-	
+
 	@Before
 	public void doSetup() {
 		authService = new AuthenticationServiceImpl();
 	}
-	
+
 	@Test
 	public void testUserSave() {
 		SignupForm signupForm = new SignupForm();
 		signupForm.setFirstName("Max");
 		signupForm.setLastName("Muster");
 		signupForm.setEmail("max@muster.ch");
-		
-		SignupForm resultForm = authService.createProfile(signupForm);
 
+		SignupForm resultForm = authService.createProfile(signupForm);
+		
 		assertEquals("Max", resultForm.getFirstName());
-		assertEquals("Muster", resultForm.getFirstName());
-		assertEquals("max@muster.ch", resultForm.getFirstName());
+		assertEquals("Muster", resultForm.getLastName());
+		assertEquals("max@muster.ch", resultForm.getEmail());
 	}
-	
+
 }
