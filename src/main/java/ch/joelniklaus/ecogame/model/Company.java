2106a66851635544bb65fd57889ab010ae7d4875
Joel Niklaus <me@joelniklaus.ch>
2015-05-21 21:08:04 +0200

    added cascade annotations

--- src/main/java/ch/joelniklaus/ecogame/model/Company.java
+++ src/main/java/ch/joelniklaus/ecogame/model/Company.java
@@ -3,6 +3,7 @@ package ch.joelniklaus.ecogame.model;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -12,31 +13,31 @@ import javax.persistence.OneToOne;
 
 @Entity
 public class Company {
-
+	
 	@Id
 	@GeneratedValue
 	protected Long id;
-
-	protected String name;
 	
-	@OneToOne
+	protected String name;
+
+	@OneToOne(cascade = CascadeType.ALL)
 	protected BankAccount bankAccount;
-
-	@OneToMany(fetch = FetchType.EAGER)
+	
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 	private List<Machine> machines = new LinkedList<Machine>();
-
+	
 	public void pay(Double amount) {
 		bankAccount.charge(amount);
 	}
-
+	
 	public void receive(Double amount) {
 		bankAccount.deposit(amount);
 	}
-
-	public Company() {
-		
-	}
 	
+	public Company() {
+
+	}
+
 	public Company(Long id, String name, BankAccount bankAccount, List<Machine> machines) {
 		super();
 		this.id = id;
